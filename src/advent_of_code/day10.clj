11431e846da956aeab53926f879567f842efadc8
Gauthier <gauthier@dataroots.io>
2020-12-10 11:03:37 +0100

    = easier to read memoization

--- src/advent_of_code/day10.clj
+++ src/advent_of_code/day10.clj
@@ -23,38 +23,36 @@
     )
   )
 
-(defn find-poss-jolt-memo []
-  (with-local-vars
-    [find-poss (memoize                                     ;trick to use memoization inside a recursive function
-                 (fn [adapters] (if (< (count adapters) 2)
-                                  1
-                                  (let [cur (first adapters)
-                                        nexts (take 3 (rest adapters))
-                                        paths (count (filter (fn [x] (<= (- x cur) 3)) nexts))
-                                        new-ad (map (fn [x] (drop (+ x 1) adapters)) (range paths))
-                                        ]
-                                    (apply + (map find-poss new-ad)))
-                                  )))]
-    (.bindRoot find-poss @find-poss)
-    @find-poss))
+(defn find-poss-jolt-memo [new-ad]
+  (memoize
+    (fn [adapters]
+      (if (< (count adapters) 2)
+        1
+        (let [cur (first adapters)
+              nexts (take 3 (rest adapters))
+              paths (count (filter (fn [x] (<= (- x cur) 3)) nexts))
+              new-ad (map (fn [x] (drop (+ x 1) adapters)) (range paths))
+              ]
+          (apply + (map find-poss-jolt-memo new-ad)))
+        )))
 
-(defn -main
-  [& args]
+  (defn -main
+    [& args]
 
-  (def data-file
-    (io/resource "inputday10.txt"))
+    (def data-file
+      (io/resource "inputday10.txt"))
 
-  (def lines (map parse-input (str/split (slurp data-file) #"\n")))
+    (def lines (map parse-input (str/split (slurp data-file) #"\n")))
 
-  (println (count lines))
-  (println (count (set lines)))                             ; no double -> can sort
+    (println (count lines))
+    (println (count (set lines)))                           ; no double -> can sort
 
-  (let [res (find-jolt (sort (conj lines 0)) 0 0)]
-    (println (* (:c-1 res) (+ (:c-3 res) 1)))               ; first part
+    (let [res (find-jolt (sort (conj lines 0)) 0 0)]
+      (println (* (:c-1 res) (+ (:c-3 res) 1)))             ; first part
+      )
+
+    (println (sort (conj lines 0)))
+    (let [res (find-poss-jolt-memo (sort (conj lines 0)))]
+      (println res)                                         ;second part
+      )
     )
-
-  (println (sort (conj lines 0)))
-  (let [res ((find-poss-jolt-memo) (sort (conj lines 0)))]
-    (println res)                                           ;second part
-    )
-  )
