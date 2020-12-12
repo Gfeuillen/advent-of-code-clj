6bd0d64eda3665d7ac865d74613eda63d10de4f0
Gauthier <gauthier@dataroots.io>
2020-12-12 18:41:26 +0100

    = apply same techniques to second solution

--- src/advent_of_code/day12.clj
+++ src/advent_of_code/day12.clj
@@ -51,44 +51,56 @@
           \L (recur rest-instr (+ degrees num) change-y change-x)
           \R (recur rest-instr (- degrees num) change-y change-x)
           \F (recur (conj rest-instr
-                                 {:instr (degree-to-dir degrees)
-                                  :n     num}) degrees change-y change-x)
+                          {:instr (degree-to-dir degrees)
+                           :n     num}) degrees change-y change-x)
           ))
       ))
 
   )
 
 
-(defn execute-prog-2 [instrs d wy wx dy dx]
-  (if (empty? instrs)
-    (+ (abs dy) (abs dx))
-    (let [c-i (first instrs)
-          i (:instr c-i)
-          nm (:n c-i)]
-      (case i
-        \N (execute-prog-2 (rest instrs) d (+ wy nm) wx dy dx)
-        \S (execute-prog-2 (rest instrs) d (- wy nm) wx dy dx)
-        \E (execute-prog-2 (rest instrs) d wy (+ wx nm) dy dx)
-        \W (execute-prog-2 (rest instrs) d wy (- wx nm) dy dx)
-        \L (case nm
-             90 (execute-prog-2 (rest instrs) d wx (- wy) dy dx)
-             180 (execute-prog-2 (rest instrs) d (- wy) (- wx) dy dx)
-             270 (execute-prog-2 (rest instrs) d (- wx) wy dy dx)
-             (execute-prog-2 (rest instrs) d wy wx dy dx))
-        \R (execute-prog-2 (conj (rest instrs)
-                                 {:instr \L
-                                  :n     (abs (- 360 nm))}) d wy wx dy dx)
-        \F (execute-prog-2 (rest instrs) d wy wx (+ dy (* nm wy)) (+ dx (* nm wx)))
-        ))
-    )
+(defn execute-prog-2 [instrs]
+  (loop [instructions instrs
+         waypoint-y 1
+         waypoint-x 10
+         change-y 0
+         change-x 0]
+    (if (empty? instructions)
+      (+ (abs change-y) (abs change-x))
+      (let [instruction (first instructions)
+            rest-instr (rest instructions)
+            num (:n instruction)]
+        (case (:instr instruction)
+          \N (recur rest-instr (+ waypoint-y num) waypoint-x change-y change-x)
+          \S (recur rest-instr (- waypoint-y num) waypoint-x change-y change-x)
+          \E (recur rest-instr waypoint-y (+ waypoint-x num) change-y change-x)
+          \W (recur rest-instr waypoint-y (- waypoint-x num) change-y change-x)
+          \L (case num
+               90 (recur rest-instr waypoint-x (- waypoint-y) change-y change-x)
+               180 (recur rest-instr (- waypoint-y) (- waypoint-x) change-y change-x)
+               270 (recur rest-instr (- waypoint-x) waypoint-y change-y change-x)
+               (recur rest-instr waypoint-y waypoint-x change-y change-x))
+          \R (recur
+               (conj rest-instr {:instr \L :n (abs (- 360 num))})
+               waypoint-y
+               waypoint-x
+               change-y
+               change-x)
+          \F (recur
+               rest-instr
+               waypoint-y
+               waypoint-x
+               (+ change-y (* num waypoint-y))
+               (+ change-x (* num waypoint-x)))
+          ))
+      ))
   )
 
 (defn -main
   [& args]
   (let [sol-1 (comp execute-prog (partial map parse-line) read-lines)
-        lines (read-lines "inputday12.txt")
-        input (map parse-line lines)]
+        sol-2 (comp execute-prog-2 (partial map parse-line) read-lines)]
     (println (sol-1 "inputday12.txt"))
-    (println (execute-prog-2 input 0 1 10 0 0))
+    (println (sol-2 "inputday12.txt"))
     )
   )
