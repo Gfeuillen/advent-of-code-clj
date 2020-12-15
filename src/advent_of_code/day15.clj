4a3924bedac56d359a687ce3ed1a3767da004cb0
Gauthier <gauthier@dataroots.io>
2020-12-15 23:16:27 +0100

    + day 15

---
+++ src/advent_of_code/day15.clj
@@ -0,0 +1,47 @@
+(ns advent-of-code.day15
+  (:gen-class)
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str])
+  (:require [clojure.set])
+  (:require [clojure.core.reducers :as r])
+  (:require [clojure.math.numeric-tower :as math])
+  (:require [clojure.math.combinatorics :as combo]))
+
+(defn infinite-play [mem current i]
+  (let [last-seen-i (get mem current 0)]
+    ;(println mem current i)
+    (if (not (contains? mem current))
+      (let [new-mem (assoc mem current i)]
+        (do
+          ;(println "not seen")
+          (lazy-seq (cons 0 (infinite-play new-mem 0 (inc i))))))
+      (let [last-seen-i (get mem current)
+            diff (- i last-seen-i)
+            new-mem (assoc mem current i)]
+        (do
+          ;(println "already seen")
+          (lazy-seq (cons diff (infinite-play new-mem diff (inc i)))))
+        )
+      ))
+  )
+
+(defn sol-1 [input n]
+  (if (<= n (count input))
+    (get input (dec n))
+    (nth
+      (infinite-play
+        (apply merge (map-indexed (fn [i v] {v i}) (drop-last input)))
+        (last input)
+        (dec (count input)))
+      (- n (inc (count input)))))
+  )
+(defn sol-2 [] identity)
+
+(defn -main
+  [& args]
+  (println (sol-1 [1 3 2] 2020))
+  (println (sol-1 [2 1 3] 2020))
+  (println (sol-1 [1 2 3] 2020))
+  (println (sol-1 [20 0 1 11 6 3] 2020))
+  (println (sol-1 [20 0 1 11 6 3] 30000000))
+  )
